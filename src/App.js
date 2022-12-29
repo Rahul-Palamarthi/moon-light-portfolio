de0774a556d175db9a21167e6753d2d14e69d489
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2022-12-29 21:29:08 +0530

    updated the app files(js, css)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import "./App.css";
+import Header from "./components/Header";
+import Home from "./Pages/Home";
+import AboutMe from "./Pages/AboutMe";
+import Passion from "./Pages/Passion";
+import Skills from "./Pages/Skills";
+import Works from "./Pages/Works";
+import ContactUs from "./Pages/ContactUs";
+import Footer from "./components/Footer";
 
 function App() {
     return (
         <>
-            <h2>hello world</h2>
+            <Header />
+            <main>
+                <Home />
+                <AboutMe />
+                <Passion />
+                <Skills />
+                <Works />
+                <ContactUs />
+            </main>
+            <Footer />
         </>
     );
 }
